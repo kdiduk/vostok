9af408c6dabf36592c09829e42e3e45c7b98d26b
Kirill Diduk <kirill.diduk@mail.ru>
2023-06-18 21:06:11 +0200

    Use built-in emacs style settings instead of EditorConfig

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode . ((c-file-style . "stroustrup")
+	      (indent-tabs-mode . nil)
+	      (c-file-offsets . ((arglist-intro . ++)
+				 (arglist-cont-nonempty . ++)
+				 (arglist-close . 0)
+				 (brace-list-intro . 0)
+				 (innamespace . 0)
+				 (statement-cont . ++)
+				 (inline-open . 0))))))
